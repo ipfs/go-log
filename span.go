opentracing/basictracer-go

Illustrate the ramifications of ot.SpanContext
Benchmark diffs:

```
bhs@bhs-personal:~/git/lightstep/go/src$ ../bin/benchcmp /tmp/before /tmp/after
benchmark                                                    old ns/op     new ns/op     delta
BenchmarkSpan_Empty-8                                        629           632           +0.48%
BenchmarkSpan_100Events-8                                    35847         35271         -1.61%
BenchmarkSpan_1000Events-8                                   35587         35173         -1.16%
BenchmarkSpan_100Tags-8                                      43618         44145         +1.21%
BenchmarkSpan_1000Tags-8                                     44063         43838         -0.51%
BenchmarkSpan_100BaggageItems-8                              145397        32579         -77.59%
BenchmarkTrimmedSpan_100Events_100Tags_100BaggageItems-8     168957        79144         -53.16%
BenchmarkInject_TextMap_Empty-8                              2221          2292          +3.20%
BenchmarkInject_TextMap_100BaggageItems-8                    85977         87857         +2.19%
BenchmarkInject_Binary_Empty-8                               388           390           +0.52%
BenchmarkInject_Binary_100BaggageItems-8                     17861         18968         +6.20%
BenchmarkJoin_TextMap_Empty-8                                2849          2872          +0.81%
BenchmarkJoin_TextMap_100BaggageItems-8                      148235        146900        -0.90%
BenchmarkJoin_Binary_Empty-8                                 1175          1231          +4.77%
BenchmarkJoin_Binary_100BaggageItems-8                       37424         37700         +0.74%
```

I made these changes pretty casually and wonder if the huge improvements
on the BaggageItems benchmarks are due to some sort of measurement error
in the test itself (i.e., an invalid result).

span.go
@@ -1,7 +1,6 @@
 package basictracer
 
 import (
-	"fmt"
 	"sync"
 	"time"
 
@@ -14,9 +13,6 @@ import (
 // to (*opentracing.Span).Finish().
 type Span interface {
 	opentracing.Span
-
-	// Context contains trace identifiers
-	Context() Context
 
 	// Operation names the work done by this span instance
 	Operation() string
@@ -150,44 +146,12 @@ func (s *spanImpl) FinishWithOptions(opts opentracing.FinishOptions) {
 	}
 }
 
-func (s *spanImpl) SetBaggageItem(restrictedKey, val string) opentracing.Span {
-	canonicalKey, valid := opentracing.CanonicalizeBaggageKey(restrictedKey)
-	if !valid {
-		panic(fmt.Errorf("Invalid key: %q", restrictedKey))
-	}
-
-	s.Lock()
-	defer s.Unlock()
-	s.onBaggage(canonicalKey, val)
-	if s.trim() {
-		return s
-	}
-
-	if s.raw.Baggage == nil {
-		s.raw.Baggage = make(map[string]string)
-	}
-	s.raw.Baggage[canonicalKey] = val
-	return s
-}
-
-func (s *spanImpl) BaggageItem(restrictedKey string) string {
-	canonicalKey, valid := opentracing.CanonicalizeBaggageKey(restrictedKey)
-	if !valid {
-		panic(fmt.Errorf("Invalid key: %q", restrictedKey))
-	}
-
-	s.Lock()
-	defer s.Unlock()
-
-	return s.raw.Baggage[canonicalKey]
-}
-
 func (s *spanImpl) Tracer() opentracing.Tracer {
 	return s.tracer
 }
 
-func (s *spanImpl) Context() Context {
-	return s.raw.Context
+func (s *spanImpl) SpanContext() opentracing.SpanContext {
+	return &s.raw.SpanContext
 }
 
 func (s *spanImpl) Operation() string {

ipfs/go-log

span.go
@@ -1,7 +1,6 @@
 package basictracer
 
 import (
-	"fmt"
 	"sync"
 	"time"
 
@@ -14,9 +13,6 @@ import (
 // to (*opentracing.Span).Finish().
 type Span interface {
 	opentracing.Span
-
-	// Context contains trace identifiers
-	Context() Context
 
 	// Operation names the work done by this span instance
 	Operation() string
@@ -152,54 +148,12 @@ func (s *spanImpl) FinishWithOptions(opts opentracing.FinishOptions) {
 	}
 }
 
-func (s *spanImpl) SetBaggageItem(restrictedKey, val string) opentracing.Span {
-	canonicalKey, valid := opentracing.CanonicalizeBaggageKey(restrictedKey)
-	if !valid {
-		panic(fmt.Errorf("Invalid key: %q", restrictedKey))
-	}
-
-	s.Lock()
-	defer s.Unlock()
-	s.onBaggage(canonicalKey, val)
-	if s.trim() {
-		return s
-	}
-
-	if s.raw.Baggage == nil {
-		s.raw.Baggage = make(map[string]string)
-	}
-	s.raw.Baggage[canonicalKey] = val
-	return s
-}
-
-func (s *spanImpl) BaggageItem(restrictedKey string) string {
-	canonicalKey, valid := opentracing.CanonicalizeBaggageKey(restrictedKey)
-	if !valid {
-		panic(fmt.Errorf("Invalid key: %q", restrictedKey))
-	}
-
-	s.Lock()
-	defer s.Unlock()
-
-	return s.raw.Baggage[canonicalKey]
-}
-
-func (s *spanImpl) ForeachBaggageItem(handler func(k, v string) bool) {
-	s.Lock()
-	defer s.Unlock()
-	for k, v := range s.raw.Baggage {
-		if !handler(k, v) {
-			break
-		}
-	}
-}
-
 func (s *spanImpl) Tracer() opentracing.Tracer {
 	return s.tracer
 }
 
-func (s *spanImpl) Context() Context {
-	return s.raw.Context
+func (s *spanImpl) Context() opentracing.SpanContext {
+	return &s.raw.SpanContext
 }
 
 func (s *spanImpl) Operation() string {