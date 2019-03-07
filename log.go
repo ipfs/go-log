14558334f69946b558f11dec8319159f2683f5fe
frrist <forrest@protocol.ai>
2019-03-07 09:30:58 -0800

    remove deprecated methods

--- log.go
+++ log.go
@@ -6,12 +6,9 @@ package log
 import (
 	"bytes"
 	"context"
-	"encoding/json"
 	"path"
 	"runtime"
 	"time"
-
-	writer "github.com/ipfs/go-log/writer"
 
 	opentrace "github.com/opentracing/opentracing-go"
 	otExt "github.com/opentracing/opentracing-go/ext"
@@ -40,25 +37,6 @@ type StandardLogger interface {
 // containing structured metadata
 type EventLogger interface {
 	StandardLogger
-
-	// Event merges structured data from the provided inputs into a single
-	// machine-readable log event.
-	//
-	// If the context contains metadata, a copy of this is used as the base
-	// metadata accumulator.
-	//
-	// If one or more loggable objects are provided, these are deep-merged into base blob.
-	//
-	// Next, the event name is added to the blob under the key "event". If
-	// the key "event" already exists, it will be over-written.
-	//
-	// Finally the timestamp and package name are added to the accumulator and
-	// the metadata is logged.
-	// DEPRECATED
-	Event(ctx context.Context, event string, m ...Loggable)
-
-	// DEPRECATED
-	EventBegin(ctx context.Context, event string, m ...Loggable) *EventInProgress
 
 	// Start starts an opentracing span with `name`, using
 	// any Span found within `ctx` as a ChildOfRef. If no such parent could be
@@ -273,130 +251,9 @@ func deserializeContext(bCtx []byte) (opentrace.SpanContext, error) {
 	return spanContext, nil
 }
 
-// DEPRECATED use `Start`
-func (el *eventLogger) EventBegin(ctx context.Context, event string, metadata ...Loggable) *EventInProgress {
-	ctx = el.Start(ctx, event)
-
-	for _, m := range metadata {
-		for l, v := range m.Loggable() {
-			el.LogKV(ctx, l, v)
-		}
-	}
-
-	eip := &EventInProgress{}
-	eip.doneFunc = func(additional []Loggable) {
-		// anything added during the operation
-		// e.g. deprecated methods event.Append(...) or event.SetError(...)
-		for _, m := range eip.loggables {
-			for l, v := range m.Loggable() {
-				el.LogKV(ctx, l, v)
-			}
-		}
-		el.Finish(ctx)
-	}
-	return eip
-}
-
 type activeEventKeyType struct{}
 
 var activeEventKey = activeEventKeyType{}
-
-// DEPRECATED use `Start`
-func (el *eventLogger) Event(ctx context.Context, event string, metadata ...Loggable) {
-
-	// short circuit if theres nothing to write to
-	if !writer.WriterGroup.Active() {
-		return
-	}
-
-	// Collect loggables for later logging
-	var loggables []Loggable
-
-	// get any existing metadata from the context
-	existing, err := MetadataFromContext(ctx)
-	if err != nil {
-		existing = Metadata{}
-	}
-	loggables = append(loggables, existing)
-
-	for _, datum := range metadata {
-		loggables = append(loggables, datum)
-	}
-
-	e := entry{
-		loggables: loggables,
-		system:    el.system,
-		event:     event,
-	}
-
-	accum := Metadata{}
-	for _, loggable := range e.loggables {
-		accum = DeepMerge(accum, loggable.Loggable())
-	}
-
-	// apply final attributes to reserved keys
-	// TODO accum["level"] = level
-	accum["event"] = e.event
-	accum["system"] = e.system
-	accum["time"] = FormatRFC3339(time.Now())
-
-	var buf bytes.Buffer
-	encoder := json.NewEncoder(&buf)
-	encoder.SetEscapeHTML(false)
-	err = encoder.Encode(accum)
-	if err != nil {
-		el.Errorf("ERROR FORMATTING EVENT ENTRY: %s", err)
-		return
-	}
-
-	writer.WriterGroup.Write(buf.Bytes())
-}
-
-// DEPRECATED
-// EventInProgress represent and event which is happening
-type EventInProgress struct {
-	loggables []Loggable
-	doneFunc  func([]Loggable)
-}
-
-// DEPRECATED use `LogKV` or `SetTag`
-// Append adds loggables to be included in the call to Done
-func (eip *EventInProgress) Append(l Loggable) {
-	eip.loggables = append(eip.loggables, l)
-}
-
-// DEPRECATED use `SetError(ctx, error)`
-// SetError includes the provided error
-func (eip *EventInProgress) SetError(err error) {
-	eip.loggables = append(eip.loggables, LoggableMap{
-		"error": err.Error(),
-	})
-}
-
-// DEPRECATED use `Finish`
-// Done creates a new Event entry that includes the duration and appended
-// loggables.
-func (eip *EventInProgress) Done() {
-	eip.doneFunc(eip.loggables) // create final event with extra data
-}
-
-// DEPRECATED use `FinishWithErr`
-// DoneWithErr creates a new Event entry that includes the duration and appended
-// loggables. DoneWithErr accepts an error, if err is non-nil, it is set on
-// the EventInProgress. Otherwise the logic is the same as the `Done()` method
-func (eip *EventInProgress) DoneWithErr(err error) {
-	if err != nil {
-		eip.SetError(err)
-	}
-	eip.doneFunc(eip.loggables)
-}
-
-// DEPRECATED use `Finish`
-// Close is an alias for done
-func (eip *EventInProgress) Close() error {
-	eip.Done()
-	return nil
-}
 
 // FormatRFC3339 returns the given time in UTC with RFC3999Nano format.
 func FormatRFC3339(t time.Time) string {
