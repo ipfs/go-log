129fa80a731583d54700807a7bfa6ddd3d405735
Jakub Sztandera <kubuxu@protonmail.ch>
2019-08-28 19:36:19 +0200

    Cleanup stringToZap
    License: MIT
    Signed-off-by: Jakub Sztandera <kubuxu@protonmail.ch>

--- levels.go
+++ levels.go
@@ -4,9 +4,6 @@ import "go.uber.org/zap/zapcore"
 
 func stringToZap(level string) (zapcore.Level, error) {
 	lvl := zapcore.DebugLevel
-	err := (&lvl).Set(level)
-	if err != nil {
-		return zapcore.DebugLevel, err
-	}
-	return lvl, nil
+	err := lvl.Set(level)
+	return lvl, err
 }
